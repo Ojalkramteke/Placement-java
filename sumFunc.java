d7a47ea5f96e153aa99c7a1df1fe6aea02dc7890
Ojalkramteke <ojalramteke@gmail.com>
2026-07-21 20:02:46 +0530

    Day 4: Functions Basics

---
+++ sumFunc.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class sumFunc{
+	
+	public static int Sum(int a, int b){
+		int sum = a+b;
+		return sum;
+	}
+
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int sum = Sum(a, b);
+	System.out.println("The sum of the two numbers is " + sum);
+	
+}
+}
+
+
+
+
