6bc71e460bf536ae54a49e05c8557ac35aee03ef
Ojalkramteke <ojalramteke@gmail.com>
2026-07-23 18:18:00 +0530

    Day 5: Functions Basics

--- factorialfunc.java
+++ factorialfunc.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
-public class factorialfunc{
-    public static void factoriall(int n){
-int factorial=1;
-    for(int i =1; i>=1;i--){
-        factorial= factorial*i;
+
+public class factorialfunc {
+
+    public static void factorial(int n) {
+        int factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial of the number is: " + factorial);
     }
-    System.out.println("Factorial of the number is: " + factorial);
-    }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
         int n = sc.nextInt();
-        factoriall(n);
+
+        factorial(n);
+
+        sc.close();
+    }
 }
-}
-    
